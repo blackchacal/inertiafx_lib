1a739f0e1729b0f778b2b1c583a467347652fc5f
blackchacal <ribeiro.tonet@gmail.com>
2024-10-06 16:53:15 +0100

    Add main.cpp and CMake.

---
+++ Core/Inc/InertiaFXConfig.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * InertiaFX - Physics Engine                                                 *
+ *                                                                            *
+ * \brief Main project configuration file.                                    *
+ *                                                                            *
+ * \author blackchacal <ribeiro.tonet@gmail.com>                              *
+ * \date October, 6th 2024                                                    *
+ *                                                                            *
+ * \copyright 2024-NOW. Ricardo Tonet. All rights reserved.                   *
+ *****************************************************************************/
+
+/* Project version is defined in CMakeLists.txt */
+#define InertiaFX_VERSION_MAJOR @InertiaFX_VERSION_MAJOR@
+#define InertiaFX_VERSION_MINOR @InertiaFX_VERSION_MINOR@
+#define InertiaFX_VERSION_PATCH @InertiaFX_VERSION_PATCH@
